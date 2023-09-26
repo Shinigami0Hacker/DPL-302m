b0680a5e71c94c2317f0930e5761db7834f24b6e
hocdichoiitthoi <145585911+hocdichoiitthoi@users.noreply.github.com>
2023-09-26 23:39:56 +0700

    Add files via upload

---
+++ Python_Basics_with_Numpy.py
@@ -0,0 +1,262 @@
+#E1
+# (≈ 1 line of code)
+#test = 'Hello World'
+# YOUR CODE STARTS HERE
+test = 'Hello World'
+# YOUR CODE ENDS HERE
+print ("test: " + test)
+
+#E2
+import math
+from public_tests import *
+
+def basic_sigmoid(x):
+    """
+    #Compute sigmoid of x.
+
+    #Arguments:
+    x -- A scalar
+
+    #Return:
+    s -- sigmoid(x)
+    """
+    # (≈ 1 line of code)
+    # s = 
+    # YOUR CODE STARTS HERE
+    
+    s=1 / (1 + math.exp(-x))
+    
+    # YOUR CODE ENDS HERE
+    
+    return s
+
+
+print("basic_sigmoid(1) = " + str(basic_sigmoid(1)))
+
+basic_sigmoid_test(basic_sigmoid)
+
+
+#E3
+def sigmoid(x):
+    """
+    Compute the sigmoid of x
+
+    Arguments:
+    x -- A scalar or numpy array of any size
+
+    Return:
+    s -- sigmoid(x)
+    """
+    
+    # (≈ 1 line of code)
+    # s = 
+    # YOUR CODE STARTS HERE
+    s = 1/(1+np.exp(-x))
+    
+    # YOUR CODE ENDS HERE
+    
+    return s
+
+
+t_x = np.array([1, 2, 3])
+print("sigmoid(t_x) = " + str(sigmoid(t_x)))
+
+sigmoid_test(sigmoid)
+
+
+#E4
+def sigmoid_derivative(x):
+    """
+    Compute the gradient (also called the slope or derivative) of the sigmoid function with respect to its input x.
+    You can store the output of the sigmoid function into variables and then use it to calculate the gradient.
+    
+    Arguments:
+    x -- A scalar or numpy array
+
+    Return:
+    ds -- Your computed gradient.
+    """
+    
+    #(≈ 2 lines of code)
+    # s = 
+    # ds = 
+    # YOUR CODE STARTS HERE
+    s = sigmoid(x)
+    ds =s*(1-s)
+    
+    # YOUR CODE ENDS HERE
+    
+    return ds
+
+t_x = np.array([1, 2, 3])
+print ("sigmoid_derivative(t_x) = " + str(sigmoid_derivative(t_x)))
+
+sigmoid_derivative_test(sigmoid_derivative)
+
+
+#E5
+def image2vector(image):
+    """
+    Argument:
+    image -- a numpy array of shape (length, height, depth)
+    
+    Returns:
+    v -- a vector of shape (length*height*depth, 1)
+    """
+    
+    # (≈ 1 line of code)
+    # v =
+    # YOUR CODE STARTS HERE
+    v = image.reshape(image.shape[0]*image.shape[1]*image.shape[2],1)
+    
+    # YOUR CODE ENDS HERE
+    
+    return v
+
+# This is a 3 by 3 by 2 array, typically images will be (num_px_x, num_px_y,3) where 3 represents the RGB values
+t_image = np.array([[[ 0.67826139,  0.29380381],
+                     [ 0.90714982,  0.52835647],
+                     [ 0.4215251 ,  0.45017551]],
+
+                   [[ 0.92814219,  0.96677647],
+                    [ 0.85304703,  0.52351845],
+                    [ 0.19981397,  0.27417313]],
+
+                   [[ 0.60659855,  0.00533165],
+                    [ 0.10820313,  0.49978937],
+                    [ 0.34144279,  0.94630077]]])
+
+print ("image2vector(image) = " + str(image2vector(t_image)))
+
+image2vector_test(image2vector)
+
+
+#E6
+def normalize_rows(x):
+    """
+    Implement a function that normalizes each row of the matrix x (to have unit length).
+    
+    Argument:
+    x -- A numpy matrix of shape (n, m)
+    
+    Returns:
+    x -- The normalized (by row) numpy matrix. You are allowed to modify x.
+    """
+    
+    #(≈ 2 lines of code)
+    # Compute x_norm as the norm 2 of x. Use np.linalg.norm(..., ord = 2, axis = ..., keepdims = True)
+    # x_norm =
+    # Divide x by its norm.
+    # x =
+    # YOUR CODE STARTS HERE
+    x_norm = np.linalg.norm(x, axis = 1, keepdims = True)
+    x = x/x_norm
+    # YOUR CODE ENDS HERE
+
+    return x
+
+x = np.array([[0, 3, 4],
+              [1, 6, 4]])
+print("normalizeRows(x) = " + str(normalize_rows(x)))
+
+normalizeRows_test(normalize_rows)
+
+
+#E7
+def softmax(x):
+    """Calculates the softmax for each row of the input x.
+
+    Your code should work for a row vector and also for matrices of shape (m,n).
+
+    Argument:
+    x -- A numpy matrix of shape (m,n)
+
+    Returns:
+    s -- A numpy matrix equal to the softmax of x, of shape (m,n)
+    """
+    
+    #(≈ 3 lines of code)
+    # Apply exp() element-wise to x. Use np.exp(...).
+    # x_exp = ...
+
+    # Create a vector x_sum that sums each row of x_exp. Use np.sum(..., axis = 1, keepdims = True).
+    # x_sum = ...
+    
+    # Compute softmax(x) by dividing x_exp by x_sum. It should automatically use numpy broadcasting.
+    # s = ...
+    
+    # YOUR CODE STARTS HERE
+    
+    x_exp = np.exp(x)
+    x_sum = np.sum(x_exp,axis=1,keepdims=True)
+    s = x_emp/x_sum
+    
+    # YOUR CODE ENDS HERE
+    
+    return s
+
+t_x = np.array([[9, 2, 5, 0, 0],
+                [7, 5, 0, 0 ,0]])
+print("softmax(x) = " + str(softmax(t_x)))
+
+softmax_test(softmax)
+
+
+
+
+
+#E8
+def L1(yhat, y):
+    """
+    Arguments:
+    yhat -- vector of size m (predicted labels)
+    y -- vector of size m (true labels)
+    
+    Returns:
+    loss -- the value of the L1 loss function defined above
+    """
+    
+    #(≈ 1 line of code)
+    # loss = 
+    # YOUR CODE STARTS HERE
+    loss = np.sum(abs(y-yhat))
+    
+    # YOUR CODE ENDS HERE
+    
+    return loss
+
+yhat = np.array([.9, 0.2, 0.1, .4, .9])
+y = np.array([1, 0, 0, 1, 1])
+print("L1 = " + str(L1(yhat, y)))
+
+L1_test(L1)
+
+
+
+#E9
+def L2(yhat, y):
+    """
+    Arguments:
+    yhat -- vector of size m (predicted labels)
+    y -- vector of size m (true labels)
+    
+    Returns:
+    loss -- the value of the L2 loss function defined above
+    """
+    
+    #(≈ 1 line of code)
+    # loss = ...
+    # YOUR CODE STARTS HERE
+    loss = np.sum(np.dot(y-yhat, y-yhat))
+    
+    # YOUR CODE ENDS HERE
+    
+    return loss
+
+
+yhat = np.array([.9, 0.2, 0.1, .4, .9])
+y = np.array([1, 0, 0, 1, 1])
+
+print("L2 = " + str(L2(yhat, y)))
+
+L2_test(L2)
