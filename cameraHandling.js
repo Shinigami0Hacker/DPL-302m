3733ae055d42c1d20d90dbfb9f164ad9f82f77cd
vinhseggs <vinhnhse170557@fpt.edu.vn>
2023-10-02 16:56:14 +0700

    update

--- cameraHandling.js
+++ cameraHandling.js
@@ -1,24 +1,3 @@
-pushUserInput = () => {
-
-    let user_input = document.getElementById('user_input')
-    let chat_section = document.getElementById('chat_section')
-    let post = document.createElement("p")
-
-    if (user_input.value === ""){
-        return
-    }
-
-    post.textContent = user_input.value
-    post.id = "post_chat"
-
-    chat_section.appendChild(post);
-    user_input.value = ""
-}
-
-processModel = () => {
-    console.log("Hello world")
-}
-
 const videoElement = document.getElementById("camera");
 const startButton = document.getElementById("startButton");
 const stopButton = document.getElementById("stopButton");
