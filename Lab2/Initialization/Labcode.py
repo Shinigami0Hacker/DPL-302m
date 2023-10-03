0b96ccf568e69ad4a7449e6ecdd0760618bb8691
bethie-03 <phuongtrang4583@gmail.com>
2023-10-03 14:37:31 +0700

    add Initialize labcode

---
+++ Lab2/Initialization/Labcode.py
@@ -0,0 +1,69 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn
+import sklearn.datasets
+from public_tests import *
+from init_utils import sigmoid, relu, compute_loss, forward_propagation, backward_propagation
+from init_utils import update_parameters, predict, load_dataset, plot_decision_boundary, predict_dec
+
+train_X, train_Y, test_X, test_Y = load_dataset()
+
+#EXCERCISE1
+# GRADED FUNCTION: initialize_parameters_zeros 
+def initialize_parameters_zeros(layers_dims):
+    parameters = {}
+    L = len(layers_dims)            
+    
+    for l in range(1, L):
+        parameters['W' + str(l)] = np.zeros((layers_dims[l], layers_dims[l - 1]))
+        parameters['b' + str(l)] = np.zeros((layers_dims[l], 1))
+    return parameters
+print('Exercise 1') 
+parameters = initialize_parameters_zeros([3, 2, 1])
+print("W1 = " + str(parameters["W1"]))
+print("b1 = " + str(parameters["b1"]))
+print("W2 = " + str(parameters["W2"]))
+print("b2 = " + str(parameters["b2"]))
+initialize_parameters_zeros_test(initialize_parameters_zeros)
+print('------------------------------------')
+
+#EXCERCISE 2
+# GRADED FUNCTION: initialize_parameters_random
+def initialize_parameters_random(layers_dims):
+    np.random.seed(3)               
+    parameters = {}
+    L = len(layers_dims)            
+    
+    for l in range(1, L):
+        parameters['W' + str(l)] = np.random.randn(layers_dims[l], layers_dims[l - 1]) * 10
+        parameters['b' + str(l)] = np.zeros((layers_dims[l], 1))
+    return parameters
+print('Exercise 2') 
+parameters = initialize_parameters_random([3, 2, 1])
+print("W1 = " + str(parameters["W1"]))
+print("b1 = " + str(parameters["b1"]))
+print("W2 = " + str(parameters["W2"]))
+print("b2 = " + str(parameters["b2"]))
+initialize_parameters_random_test(initialize_parameters_random)
+print('------------------------------------')
+
+#EXCERCISE 3
+# GRADED FUNCTION: initialize_parameters_he
+def initialize_parameters_he(layers_dims):
+    np.random.seed(3)
+    parameters = {}
+    L = len(layers_dims) - 1 
+     
+    for l in range(1, L + 1):
+        parameters['W' + str(l)] = np.random.randn(layers_dims[l], layers_dims[l - 1]) * np.sqrt(2 / layers_dims[l - 1])
+        parameters['b' + str(l)] = np.zeros((layers_dims[l], 1))     
+    return parameters
+print('Exercise 3') 
+parameters = initialize_parameters_he([2, 4, 1])
+print("W1 = " + str(parameters["W1"]))
+print("b1 = " + str(parameters["b1"]))
+print("W2 = " + str(parameters["W2"]))
+print("b2 = " + str(parameters["b2"]))
+
+initialize_parameters_he_test(initialize_parameters_he)
+print('--------------The End------------------')
