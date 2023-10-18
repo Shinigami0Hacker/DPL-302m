956141ce37d4c437639e416a6ca5ae7c087194f2
bethie-03 <122683503+bethie-03@users.noreply.github.com>
2023-10-18 07:54:56 +0700

    Add files via upload

---
+++ Lab4/Autonomous_driving_application_Car_detection/Labcode.py
@@ -0,0 +1,212 @@
+import argparse
+import os
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import imshow
+import scipy.io
+import scipy.misc
+import numpy as np
+import pandas as pd
+import PIL
+from PIL import ImageFont, ImageDraw, Image
+import tensorflow as tf
+from tensorflow.python.framework.ops import EagerTensor
+
+from tensorflow.keras.models import load_model
+from yad2k.models.keras_yolo import yolo_head
+from yad2k.utils.utils import draw_boxes, get_colors_for_classes, scale_boxes, read_classes, read_anchors, preprocess_image
+
+# UNQ_C1 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
+# GRADED FUNCTION: yolo_filter_boxes
+
+def yolo_filter_boxes(boxes, box_confidence, box_class_probs, threshold = .6):
+    box_scores = box_confidence * box_class_probs
+    box_classes =  tf.math.argmax(box_scores, axis = -1)
+    box_class_scores = tf.math.reduce_max(box_scores, axis = -1)
+    filtering_mask = box_class_scores >= threshold
+    scores = tf.boolean_mask(box_class_scores, filtering_mask, axis=None, name='boolean_mask')
+    boxes = tf.boolean_mask(boxes, filtering_mask, axis=None, name='boolean_mask')
+    classes = tf.boolean_mask(box_classes, filtering_mask, axis=None, name='boolean_mask')
+    
+    return scores, boxes, classes
+
+# BEGIN UNIT TEST
+tf.random.set_seed(10)
+box_confidence = tf.random.normal([19, 19, 5, 1], mean=1, stddev=4, seed = 1)
+boxes = tf.random.normal([19, 19, 5, 4], mean=1, stddev=4, seed = 1)
+box_class_probs = tf.random.normal([19, 19, 5, 80], mean=1, stddev=4, seed = 1)
+scores, boxes, classes = yolo_filter_boxes(boxes, box_confidence, box_class_probs, threshold = 0.5)
+print("scores[2] = " + str(scores[2].numpy()))
+print("boxes[2] = " + str(boxes[2].numpy()))
+print("classes[2] = " + str(classes[2].numpy()))
+print("scores.shape = " + str(scores.shape))
+print("boxes.shape = " + str(boxes.shape))
+print("classes.shape = " + str(classes.shape))
+
+assert type(scores) == EagerTensor, "Use tensorflow functions"
+assert type(boxes) == EagerTensor, "Use tensorflow functions"
+assert type(classes) == EagerTensor, "Use tensorflow functions"
+
+assert scores.shape == (1789,), "Wrong shape in scores"
+assert boxes.shape == (1789, 4), "Wrong shape in boxes"
+assert classes.shape == (1789,), "Wrong shape in classes"
+
+assert np.isclose(scores[2].numpy(), 9.270486), "Values are wrong on scores"
+assert np.allclose(boxes[2].numpy(), [4.6399336, 3.2303846, 4.431282, -2.202031]), "Values are wrong on boxes"
+assert classes[2].numpy() == 8, "Values are wrong on classes"
+
+print("\033[92m All tests passed!")
+
+# UNQ_C2 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
+# GRADED FUNCTION: iou
+
+def iou(box1, box2):
+    (box1_x1, box1_y1, box1_x2, box1_y2) = box1
+    (box2_x1, box2_y1, box2_x2, box2_y2) = box2
+    xi1 = max(box1_x1, box2_x1)
+    yi1 = max(box1_y1, box2_y1)
+    xi2 = min(box1_x2, box2_x2)
+    yi2 = min(box1_y2, box2_y2)
+    inter_width = xi2 - xi1
+    inter_height =  yi2 - yi1
+    inter_area = max((xi2 - xi1),0)* max((yi2 - yi1),0)
+    box1_area = (box1_x2 - box1_x1) * (box1_y2 - box1_y1)
+    box2_area = (box2_x2 - box2_x1) * (box2_y2 - box2_y1)
+    union_area = box1_area + box2_area - inter_area
+    iou = inter_area / union_area
+    
+    return iou
+
+# BEGIN UNIT TEST
+## Test case 1: boxes intersect
+box1 = (2, 1, 4, 3)
+box2 = (1, 2, 3, 4)
+
+print("iou for intersecting boxes = " + str(iou(box1, box2)))
+assert iou(box1, box2) < 1, "The intersection area must be always smaller or equal than the union area."
+assert np.isclose(iou(box1, box2), 0.14285714), "Wrong value. Check your implementation. Problem with intersecting boxes"
+
+## Test case 2: boxes do not intersect
+box1 = (1,2,3,4)
+box2 = (5,6,7,8)
+print("iou for non-intersecting boxes = " + str(iou(box1,box2)))
+assert iou(box1, box2) == 0, "Intersection must be 0"
+
+## Test case 3: boxes intersect at vertices only
+box1 = (1,1,2,2)
+box2 = (2,2,3,3)
+print("iou for boxes that only touch at vertices = " + str(iou(box1,box2)))
+assert iou(box1, box2) == 0, "Intersection at vertices must be 0"
+
+## Test case 4: boxes intersect at edge only
+box1 = (1,1,3,3)
+box2 = (2,3,3,4)
+print("iou for boxes that only touch at edges = " + str(iou(box1,box2)))
+assert iou(box1, box2) == 0, "Intersection at edges must be 0"
+
+print("\033[92m All tests passed!")
+
+# UNQ_C3 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
+# GRADED FUNCTION: yolo_non_max_suppression
+
+def yolo_non_max_suppression(scores, boxes, classes, max_boxes = 10, iou_threshold = 0.5):
+    max_boxes_tensor = tf.Variable(max_boxes, dtype='int32')     
+    nms_indices = tf.image.non_max_suppression(boxes,scores,max_boxes_tensor,iou_threshold=0.5,name=None)
+    scores = tf.gather(scores , nms_indices)
+    boxes = tf.gather(boxes, nms_indices)
+    classes = tf.gather(classes, nms_indices)
+    return scores, boxes, classes
+
+tf.random.set_seed(10)
+scores = tf.random.normal([54,], mean=1, stddev=4, seed = 1)
+boxes = tf.random.normal([54, 4], mean=1, stddev=4, seed = 1)
+classes = tf.random.normal([54,], mean=1, stddev=4, seed = 1)
+scores, boxes, classes = yolo_non_max_suppression(scores, boxes, classes)
+
+assert type(scores) == EagerTensor, "Use tensoflow functions"
+print("scores[2] = " + str(scores[2].numpy()))
+print("boxes[2] = " + str(boxes[2].numpy()))
+print("classes[2] = " + str(classes[2].numpy()))
+print("scores.shape = " + str(scores.numpy().shape))
+print("boxes.shape = " + str(boxes.numpy().shape))
+print("classes.shape = " + str(classes.numpy().shape))
+
+assert type(scores) == EagerTensor, "Use tensoflow functions"
+assert type(boxes) == EagerTensor, "Use tensoflow functions"
+assert type(classes) == EagerTensor, "Use tensoflow functions"
+
+assert scores.shape == (10,), "Wrong shape"
+assert boxes.shape == (10, 4), "Wrong shape"
+assert classes.shape == (10,), "Wrong shape"
+
+assert np.isclose(scores[2].numpy(), 8.147684), "Wrong value on scores"
+assert np.allclose(boxes[2].numpy(), [ 6.0797963, 3.743308, 1.3914018, -0.34089637]), "Wrong value on boxes"
+assert np.isclose(classes[2].numpy(), 1.7079165), "Wrong value on classes"
+
+def yolo_boxes_to_corners(box_xy, box_wh):
+    """Convert YOLO box predictions to bounding box corners."""
+    box_mins = box_xy - (box_wh / 2.)
+    box_maxes = box_xy + (box_wh / 2.)
+
+    return tf.keras.backend.concatenate([
+        box_mins[..., 1:2],  
+        box_mins[..., 0:1], 
+        box_maxes[..., 1:2], 
+        box_maxes[..., 0:1]  
+    ])
+
+def yolo_eval(yolo_outputs, image_shape = (720, 1280), max_boxes=10, score_threshold=.6, iou_threshold=.5):
+    box_xy, box_wh, box_confidence, box_class_probs = yolo_outputs
+    boxes = yolo_boxes_to_corners(box_xy, box_wh)
+    scores, boxes, classes =  yolo_filter_boxes(boxes, box_confidence, box_class_probs, threshold = score_threshold)
+    boxes = scale_boxes(boxes, image_shape)
+    scores, boxes, classes = yolo_non_max_suppression(scores, boxes, classes, max_boxes = max_boxes, iou_threshold = iou_threshold)
+    
+    return scores, boxes, classes
+
+tf.random.set_seed(10)
+yolo_outputs = (tf.random.normal([19, 19, 5, 2], mean=1, stddev=4, seed = 1),
+                tf.random.normal([19, 19, 5, 2], mean=1, stddev=4, seed = 1),
+                tf.random.normal([19, 19, 5, 1], mean=1, stddev=4, seed = 1),
+                tf.random.normal([19, 19, 5, 80], mean=1, stddev=4, seed = 1))
+scores, boxes, classes = yolo_eval(yolo_outputs)
+print("scores[2] = " + str(scores[2].numpy()))
+print("boxes[2] = " + str(boxes[2].numpy()))
+print("classes[2] = " + str(classes[2].numpy()))
+print("scores.shape = " + str(scores.numpy().shape))
+print("boxes.shape = " + str(boxes.numpy().shape))
+print("classes.shape = " + str(classes.numpy().shape))
+
+assert type(scores) == EagerTensor, "Use tensoflow functions"
+assert type(boxes) == EagerTensor, "Use tensoflow functions"
+assert type(classes) == EagerTensor, "Use tensoflow functions"
+
+assert scores.shape == (10,), "Wrong shape"
+assert boxes.shape == (10, 4), "Wrong shape"
+assert classes.shape == (10,), "Wrong shape"
+    
+assert np.isclose(scores[2].numpy(), 171.60194), "Wrong value on scores"
+assert np.allclose(boxes[2].numpy(), [-1240.3483, -3212.5881, -645.78, 2024.3052]), "Wrong value on boxes"
+assert np.isclose(classes[2].numpy(), 16), "Wrong value on classes"
+    
+print("\033[92m All tests passed!")
+
+class_names = read_classes("model_data/coco_classes.txt")
+anchors = read_anchors("model_data/yolo_anchors.txt")
+model_image_size = (608, 608) # Same as yolo_model input layer size
+yolo_model = load_model("model_data/", compile=False)
+yolo_model.summary()
+
+def predict(image_file):
+    image, image_data = preprocess_image("images/" + image_file, model_image_size = (608, 608))
+    yolo_model_outputs = yolo_model(image_data)
+    yolo_outputs = yolo_head(yolo_model_outputs, anchors, len(class_names))
+    out_scores, out_boxes, out_classes = yolo_eval(yolo_outputs, [image.size[1],  image.size[0]], 10, 0.3, 0.5)
+    print('Found {} boxes for {}'.format(len(out_boxes), "images/" + image_file))
+    colors = get_colors_for_classes(len(class_names))
+    draw_boxes(image, out_boxes, out_classes, class_names, out_scores)
+    image.save(os.path.join("out", image_file), quality=100)
+    output_image = Image.open(os.path.join("out", image_file))
+    imshow(output_image)
+
+    return out_scores, out_boxes, out_classes
+out_scores, out_boxes, out_classes = predict("test.jpg")
